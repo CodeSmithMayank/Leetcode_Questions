02c29d5a8c01e00628c91082272a55484ff3066c
CodeSmithMayank <mayankpal4812@gmail.com>
2025-10-15 20:26:18 +0530

    Intersection_Of_Two_Arrays

---
+++ Intersection_Of_Two_Arrays.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+{
+        vector<int> result ;
+        vector<int> copyvec(nums2);
+        sort(nums1.begin(),nums1.end());
+        sort(nums1.begin(),nums1.end());
+        
+        for(int i = 0 ; i < nums1.size() ; i++)
+        {
+            
+            for(int j = 0 ; j < copyvec.size() ; j++)
+            {
+                if(nums1[i]==copyvec[j])
+                {
+                    int tempno = nums1[i];
+                    for(int st = 0 ; st < result.size() ; st++)
+                    {
+                        if(result[st]==nums1[i])
+                        {
+                            tempno = - 1 ;
+                        }
+                    }
+                    if(tempno == nums1[i])
+                    {
+                        result.push_back(nums1[i]);
+                        copyvec[j] = -1;
+                        break;
+                    }
+                    
+                }
+            }
+           
+        }
+        
+        return result ;
+}
+
+int main()
+{
+    vector<int> nums1{4,9,5};
+    vector<int> nums2{9,4,9,8,4};
+    vector<int> res = intersection(nums1,nums2);
+}
