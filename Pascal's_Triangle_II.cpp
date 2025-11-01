dea450e11d46cf06d95db86a417b8ab30bd46507
CodeSmithMayank <mayankpal4812@gmail.com>
2025-11-01 14:31:20 +0530

    Pascal's_Triangle_II

---
+++ Pascal's_Triangle_II.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+  vector<int> rowvalues(int rowno , vector<int> v)
+    {
+        vector<int> ans;
+        for(int i = 0 ; i < rowno ; i++)
+        {
+            if(i == 0 || i == rowno-1)
+            {
+                ans.push_back(v[v.size()-1]);
+            }
+            else
+            {
+                ans.push_back(v[i-1]+v[i]);
+            }
+        }
+        return ans;
+    }
+    
+   vector<int> getRow(int rowIndex) {
+        vector<vector<int>> result ;
+        vector<int> tempvec;
+        for(int i = 1 ; i <= rowIndex +1 ; i++)
+        {
+          
+            if(i==1)
+            {
+                tempvec.push_back(1);
+                result.push_back(tempvec);
+                tempvec.clear();
+            }
+            else if(i==2)
+            {
+                tempvec.push_back(1);
+                tempvec.push_back(1);
+                result.push_back(tempvec);
+            }
+            else
+            {
+                vector<int> indexres = rowvalues(i,tempvec);
+                result.push_back(indexres);
+                tempvec.clear();
+                tempvec = indexres;
+            }
+        }
+        return result[rowIndex];
+    }
+
+int main()
+{
+    vector<int> result = getRow(5);
+}
