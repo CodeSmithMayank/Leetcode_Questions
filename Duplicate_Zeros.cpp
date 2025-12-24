e4a81c62f145f43f85d6f235ad6e356f1fa59a60
CodeSmithMayank <mayankpal4812@gmail.com>
2025-12-24 18:45:12 +0530

    Duplicate_Zeros

---
+++ Duplicate_Zeros.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void shtval(int i , vector<int>& a)
+{
+    int e = a.size()-1;
+    while(e>=i)
+    {
+        if(e==i)
+        {
+            a[e]=0;
+        }
+        else
+        {
+            a[e]=a[e-1];
+            
+        }
+
+        
+        e--;
+    }
+}
+
+void duplicateZeros(vector<int>& arr) 
+{
+   int s = 0;
+   while(s<arr.size())
+   {
+        if(arr[s]!=0)
+        {
+            s++;
+        }
+        else
+        {
+            shtval(s+1,arr);
+            s+=2;
+        }
+   }  
+   
+   
+}
+
+int main()
+{
+    vector<int> a{1,2,3};
+    duplicateZeros(a);   
+}
